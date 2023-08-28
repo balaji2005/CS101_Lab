7b7f1e8b139f1f2a2e3a828be14a2bd8a9fda3ab
balaji2005 <balajikaredla12345@gmail.com>
2023-08-28 22:45:49 +0530

    Edit some programs

--- chessboard.cpp
+++ chessboard.cpp
@@ -1,47 +1,63 @@
 #include <simplecpp>
 
 main_program {
-	float length = 80;
+	float length = 400;
 	float a = length/8;
 	
-	turtleSim();
+	turtleSim("Chessboard");
 	
 	penUp();
 	left(90);
+	wait(0.1);
 	forward(length/2);
+	wait(0.1);
 	left(90);
+	wait(0.1);
 	forward(length/2);
+	wait(0.1);
 	right(180);
 	penDown();
 	
 	repeat(4) {
 		forward(length);
+		wait(0.1);
 		right(90);
+		wait(0.1);
 	}
 	int n = 1;
 	repeat(3) {
 	
 		penUp();
 		forward(a);
+	wait(0.1);
 		right(90);
+	wait(0.1);
 		penDown();
 		
 		repeat(n) {
 			forward(a);
+	wait(0.1);
 			right(90);
+	wait(0.1);
 			forward(a);
+	wait(0.1);
 			left(90);
 		}
 		
 		penUp();
 		forward(a);
+	wait(0.1);
 		left(90);
+	wait(0.1);
 		penDown();
 		
 		repeat(n+1) {
 			forward(a);
+	wait(0.1);
 			left(90);
+	wait(0.1);
 			forward(a);
+	wait(0.1);
 			right(90);
 		}
 		n += 2;
@@ -49,36 +65,83 @@ main_program {
 	
 	penUp();
 	forward(a);
+	wait(0.1);
 	right(90);
+	wait(0.1);
 	penDown();
 		
 	repeat(7) {
 		forward(a);
+	wait(0.1);
 		right(90);
+	wait(0.1);
 		forward(a);
+	wait(0.1);
 		left(90);
 	}
 	
 	penUp();
 	forward(a);
+	wait(0.1);
 	left(90);
+	wait(0.1);
 	penDown();
 	
 	repeat(7) {
 		forward(a);
+	wait(0.1);
 		left(90);
+	wait(0.1);
 		forward(a);
+	wait(0.1);
 		right(90);
 	}
 	
 	forward(a);
+	wait(0.1);
 	n = 6;
 	
 	repeat(3) {
 		penUp();
 		right(90);
+	wait(0.1);
 		forward(a);
+	wait(0.1);
 		penDown();
+		right(90);
+	wait(0.1);
+		
+		repeat(n) {
+			forward(a);
+	wait(0.1);
+			left(90);
+	wait(0.1);
+			forward(a);
+	wait(0.1);
+			right(90);
+		}
+		
+		right(180);
+	wait(0.1);
+		forward(a);
+	wait(0.1);
+		left(90);
+	wait(0.1);
+		
+		repeat(n-1) {
+			forward(a);
+	wait(0.1);
+			right(90);
+	wait(0.1);
+			forward(a);
+	wait(0.1);
+			left(90);
+		}
+		
+		right(90);
+	wait(0.1);
+		
+		n -= 2;
 	}
 	
 	penUp();
--- flower.cpp
+++ flower.cpp
@@ -1,17 +1,31 @@
 #include <simplecpp>
 
 main_program {
-	turtleSim();
+	turtleSim("Flower");
+	
+	penUp();
+	forward(25);
+	penDown();
+	
+	float radius = 50.0;
 
-	repeat (8) {
-		forward(50);
-		repeat (50) {
-			forward(5);
-			right(5);
+	repeat (6) {
+		forward(25*sqrt(3));
+		repeat (240) {
+			forward(radius * PI/180);
+			right(1);
 		}
-		forward(50);
+		forward(25*sqrt(3));
 		right(180);
 	}
-
+	
+	right(90);
+	radius = 25*sqrt(3);
+	repeat (360) {
+		forward(radius * PI/180);
+		right(1);
+	}
+	penUp();
+	forward(1000);
 	wait(5);
 }
