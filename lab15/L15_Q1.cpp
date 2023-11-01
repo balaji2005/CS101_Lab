8e7e8e99fb169057660e1c027f73d61543c02bb4
Balaji <66565398+balaji2005@users.noreply.github.com>
2023-11-01 11:34:08 +0530

    Add file

--- lab15/L15_Q1.cpp
+++ lab15/L15_Q1.cpp
@@ -6,6 +6,12 @@ struct Polynomial {
     int degree;
     double coeff[101];
 
+    Polynomial() {
+        for (int i=0; i<101; i++) {
+            coeff[i] = 0;
+        }
+    }
+
     double value(double real) {
         double product = 1;
         double sum = 0;
@@ -21,17 +27,8 @@ struct Polynomial {
         int ma = max(degree, p.degree);
         r.degree = ma;
         int i;
-        for (i=0; i<= min(degree, p.degree); i++) {
+        for (i=0; i<= ma; i++) {
             r.coeff[i] = coeff[i] + p.coeff[i];
-        }
-        if (p.degree == ma) {
-            for(; i<=ma; i++) {
-                r.coeff[i] = p.coeff[i];
-            }
-        } else {
-            for(; i<=ma; i++) {
-                r.coeff[i] = coeff[i];
-            }
         }
         return r;
     }
@@ -41,18 +38,41 @@ struct Polynomial {
         int ma = max(degree, p.degree);
         r.degree = ma;
         int i;
-        for (i=0; i<= min(degree, p.degree); i++) {
+        for (i=0; i<= ma; i++) {
             r.coeff[i] = coeff[i] - p.coeff[i];
         }
-        if (p.degree == ma) {
-            for(; i<=ma; i++) {
-                r.coeff[i] = -p.coeff[i];
-            }
-        } else {
-            for(; i<=ma; i++) {
-                r.coeff[i] = coeff[i];
+        return r;
+    }
+
+    Polynomial operator*(Polynomial &p) {
+        Polynomial r;
+        int ma = degree*p.degree;
+        r.degree = ma;
+        for (int i=0; i<=ma; i++) {
+            for (int j=0; j<=i; j++) {
+                r.coeff[i] += coeff[j]*coeff[i-j];
             }
         }
         return r;
+    }
+
+    void print() {
+        cout << coeff[degree] << "x^" << degree;
+        for(int i=degree-1; i>0; i--) {
+            if (coeff[i] > 0) {
+                cout << " + " << coeff[degree] << "x^" << i;
+            } else if (coeff[i] < 0) {
+                cout << " - " << -coeff[degree] << "x" << i;
+            }
+        }
+        if (coeff[0] > 0) {
+            cout << " + " << coeff[degree] << '\n';
+        } else if (coeff[i] < 0) {
+            cout << " - " << -coeff[degree] << '\n';
+        }
+    }
+
+    void read() {
+        cout << "Enter the degree: " << '\n'
     }
 };
