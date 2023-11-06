1c58de970d216386dd119eb1c2fdbd80ce04e850
Balaji <66565398+balaji2005@users.noreply.github.com>
2023-11-07 01:12:21 +0530

    Create sorts.h

---
+++ sorts/sorts.h
@@ -0,0 +1,22 @@
+#ifndef SORTS_H
+#define SORTS_H
+
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void selection_sort_l(T[], int);
+
+template <typename T>
+void selection_sort_r(T[], int);
+
+template <typename T>
+void bubble_sort_l(T[], int);
+
+template <typename T>
+void merge_sort(T[], int);
+
+// template <typename T>
+// void bubble_sort_r(T[], int);
+
+#endif
